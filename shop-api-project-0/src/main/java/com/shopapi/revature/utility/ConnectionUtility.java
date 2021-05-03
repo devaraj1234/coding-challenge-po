e3c7f646dcd2603ba658f691ea2202c0dd8bfde3
devaraj1234 <dev.acharya286@gmail.com>
2021-05-03 16:33:26 -0500

    updated connection utility class

--- shop-api-project-0/src/main/java/com/shopapi/revature/utility/ConnectionUtility.java
+++ shop-api-project-0/src/main/java/com/shopapi/revature/utility/ConnectionUtility.java
@@ -5,15 +5,18 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnectionUtility {
-	
+
 	private static Connection conn = null;
 
 	public static Connection getConnection() throws SQLException {
+
 		String url = "jdbc:postgresql://localhost:5432/postgres";
-		String user = "postgres";
-		String password = "Durga@1234";
-		conn = DriverManager.getConnection(url, user, password);
+		String username = System.getenv("DB_USER");
+		String password = System.getenv("DB_PASSWORD");
+
+		if (conn == null || conn.isClosed()) {
+			conn = DriverManager.getConnection(url, username, password);
+		}
 		return conn;
 	}
-
 }
