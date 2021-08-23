53e2696c018f7bb163a55f23992a75bf3e13b312
devaraj1234 <38010915+devaraj1234@users.noreply.github.com>
2021-08-23 16:14:46 -0500

    Delete shop-api-project-0 directory

--- shop-api-project-0/src/main/java/com/shopapi/revature/dao/CustomerDAO.java
+++
@@ -1,9 +0,0 @@
-package com.shopapi.revature.dao;
-
-import com.shopapi.revature.model.Customer;
-
-public interface CustomerDAO extends GenericDAO<Customer>{
-	
-	public Customer getByLoginId(int loginId);
-
-}
--- shop-api-project-0/src/main/java/com/shopapi/revature/dao/UserDAO.java
+++
@@ -1,7 +0,0 @@
-package com.shopapi.revature.dao;
-
-import com.shopapi.revature.model.User;
-
-public interface UserDAO extends GenericDAO<User> {
-
-}
